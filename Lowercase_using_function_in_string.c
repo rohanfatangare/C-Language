59a521f30707e705c5bc2e59d7286ff99a26e0b4
Rohan Shrinath Fatangare <88178140+rohanfatangare@users.noreply.github.com>
2022-07-17 21:30:04 +0530

    To understand strlwr() function in C
    strlwr():
    By using this predefined function, we can convert a string into a lower case. strlwr() function requires 1 argument of type (char*) and returns (char*). When we are working with strlwr() function, from given address up to null all upper case characters are converted into lowercase.

---
+++ Lowercase_using_function_in_string.c
@@ -0,0 +1,9 @@
+#include<stdio.h> 
+#include<string.h> 
+int main() 
+{ 
+    char str[ ] = "GOOD MORNING"; 
+    //converting the given string into lowercase. 
+    printf("%s\n", strlwr (str)); 
+    return  0; 
+}
