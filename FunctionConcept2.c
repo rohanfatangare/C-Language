d9e8b7ca70c317bfcaafb1c8a48be06c69399327
Rohan Shrinath Fatangare <88178140+rohanfatangare@users.noreply.github.com>
2022-07-21 20:52:43 +0530

    Function Concept

---
+++ FunctionConcept2.c
@@ -0,0 +1,15 @@
+//How to Define function
+//1.type function w\o parameter w\o return 
+
+#include<stdio.h>  
+void greeting();
+main()
+{
+	greeting();//function call 
+}
+//function defination -> declares after main()
+void greeting()
+{
+	printf("Hello!! Good Morning \1.....");
+	printf("\n Have a nice day");
+}
